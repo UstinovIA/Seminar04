8212809dace1eea85b52da25932c917c09f56156
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-12 20:54:11 +0300

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] CreateArray(int countElements, int min, int max)
+{
+    int[] collection = new int[countElements];
+    Random rnd = new Random();
+    for (int i = 0; i < countElements; i++)
+    {
+        collection[i] = rnd.Next(min, max);
+    }
+    return collection;
+}
+
+void PrintArray(int[] collection)
+{
+    Console.Write("[");
+    for (int i = 0; i < collection.Length - 1; i++)
+    {
+        Console.Write($"{collection[i]}, ");
+    }
+    Console.Write($"{collection[collection.Length - 1]}]");
+}
+
+int[] array = CreateArray(8, 0, 100);
+PrintArray(array);
